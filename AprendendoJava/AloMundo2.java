97d558c7adc28b9bc080c7112b0d6413b639325d
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-04-30 16:17:51 -0300

    adicionei mais linhas

--- AprendendoJava/AloMundo2.java
+++ AprendendoJava/AloMundo2.java
@@ -1,10 +1,27 @@
 package AprendendoJava;
 
-public class AloMundo2 {
+    public class AloMundo2 {
 
-    public static void main(String[] args) {
-        System.out.printf("%n%n%n");
-        System.out.printf("%s%n%n%n%n", "Alo mundo!!");
+        public static void main(String[] args) {
+
+            int n1=10;
+            int n2=20;
+            int n3=30;
+            int res=n1+n2+n3;
+            String nome="Marina";
+            int num=50;
+
+            System.out.printf("%n%n%n");
+            System.out.printf("%s%n%n", "Alo mundo!!");
+
+            System.out.print("CBF Cursos\n");
+            System.out.println("Curso de Java");
+            System.out.printf("Canal: %s%nAno: %d%n","CBF Cursos",2021);
+            System.out.printf("Valor da variavel num: %d%nNome: %s%n",num,nome);
+            System.out.printf("Valor da soma: %d",res);
+
+
+
+        }
+        
     }
-    
-}
