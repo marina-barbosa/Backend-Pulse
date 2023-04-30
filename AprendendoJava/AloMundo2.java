8e51146ee89ab141f972cb0ef346132440faf50b
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-04-30 18:02:39 -0300

    novo arquivo

--- AprendendoJava/AloMundo2.java
+++ AprendendoJava/AloMundo2.java
@@ -17,10 +17,21 @@ package AprendendoJava;
             System.out.print("CBF Cursos\n");
             System.out.println("Curso de Java");
             System.out.printf("Canal: %s%nAno: %d%n","CBF Cursos",2021);
+            System.out.printf("%s - %d%n","CBF Cursos",2021);
             System.out.printf("Valor da variavel num: %d%nNome: %s%n",num,nome);
             System.out.printf("Valor da soma: %d",res);
 
 
+
+               /*     comando de impressão
+
+               %n ou \n quebra de linha
+               print - nao executa quebra de linha
+               pra quebrar linha com print use \n
+               println - executa quebra de linha no final
+                
+                printf - A função printf (abreviatura de print formatted) exibe na tela do terminal uma lista formatada de números, caracteres, strings, etc. O primeiro argumento da função é uma string que especifica o formato da impressão.       */
+                
 
         }
         
---
+++ AprendendoJava/praticandoprint.java
@@ -0,0 +1,10 @@
+package AprendendoJava;
+
+public class praticandoprint {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf("Praticando e explicando os tipos de 'prints'.\n");
+    }
+}
