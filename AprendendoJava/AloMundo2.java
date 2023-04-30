1250952f7c66fed86552491caf5a66f5c7822fa1
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-04-30 15:19:36 -0300

    adicionei mais quebra de linha

--- AprendendoJava/AloMundo2.java
+++ AprendendoJava/AloMundo2.java
@@ -3,7 +3,8 @@ package AprendendoJava;
 public class AloMundo2 {
 
     public static void main(String[] args) {
-        System.out.printf("%s%n", "Alo mundo!!");
+        System.out.printf("%n%n%n");
+        System.out.printf("%s%n%n%n%n", "Alo mundo!!");
     }
     
 }
