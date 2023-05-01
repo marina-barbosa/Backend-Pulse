f377e05b906ab7d2cd194ceaa2c7d5cb776ca78d
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-05-01 05:28:56 -0300

    organizando arquivos

---
+++ aprendendojava/AloMundo2.java
@@ -0,0 +1,28 @@
+package aprendendojava;
+
+    public class AloMundo2 {
+
+        public static void main(String[] args) {
+
+            int n1=10;
+            int n2=20;
+            int n3=30;
+            int res=n1+n2+n3;
+            String nome="Marina";
+            int num=50;
+
+            System.out.printf("\n\n\n");
+            System.out.printf("%s\n\n", "Alo mundo!!");
+
+            System.out.print("CBF Cursos\n");
+            System.out.println("Curso de Java");
+            System.out.printf("Canal: %s\nAno: %d\n","CBF Cursos",2021);
+            System.out.printf("%s - %d\n","CBF Cursos",2021);
+            System.out.printf("Valor da variavel num: %d\nNome: %s\n",num,nome);
+            System.out.printf("Valor da soma: %d\n\n\n",res);
+              
+                
+
+        }
+        
+    }
---
+++ aprendendojava/AulaSwitch.java
@@ -0,0 +1,26 @@
+public class AulaSwitch {
+
+    public static void main(String[] args) {
+        
+        int pos=3;
+
+        switch(pos){
+            case 1:
+                System.out.println("Primeiro Lugar!!");
+                break;
+            case 2:
+                System.out.println("Segundo Lugar!");
+                break;
+            case 3:
+                System.out.println("Terceiro Lugar!");
+                break;
+            case 4: case 5: case 6:
+                System.out.println("Premio de Participação.");
+                break;
+            default:
+                System.out.println("Não subiu no podio.");
+                break;
+        }
+    }
+    
+}
