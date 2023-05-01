bd2a050cf9ba063021f7b5750fe76c4f0560e69a
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-05-01 05:32:49 -0300

    add package

--- aprendendojava/AulaIfElse.java
+++ aprendendojava/AulaIfElse.java
@@ -1,3 +1,4 @@
+package aprendendojava;
 public class AulaIfElse {
 
     public static void main(String[] args) {
--- aprendendojava/AulaSwitch.java
+++ aprendendojava/AulaSwitch.java
@@ -1,3 +1,4 @@
+package aprendendojava;
 public class AulaSwitch {
 
     public static void main(String[] args) {
