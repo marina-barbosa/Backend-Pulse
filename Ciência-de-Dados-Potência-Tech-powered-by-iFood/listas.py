cca79f6d6d9523abe6b15773c28bd618dda729c5
Marina Barbosa <marinabarbosa.exp@gmail.com>
2023-07-31 16:46:30 -0300

    listas

---
+++ Ciência-de-Dados-Potência-Tech-powered-by-iFood/listas.py
@@ -0,0 +1,171 @@
+# LISTAS
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+print(frutas[-1])
+print(frutas[-3])
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
+
+# MATRIZ
+matriz = [
+    [1, "a", 2],
+    ["b", 3, 4],
+    [6, 5, "c"]
+]
+
+print(matriz[0])  # [1, "a", 2]
+print(matriz[0][0])  # 1
+print(matriz[0][-1])  # 2
+print(matriz[-1][-1])  # "c"
+
+#FATIAMENTO
+
+lista = ["p", "y", "t", "h", "o", "n"]
+
+print(lista[2:])  # ["t", "h", "o", "n"]
+print(lista[:2])  # ["p", "y"]
+print(lista[1:3])  # ["y", "t"]
+print(lista[0:3:2])  # ["p", "t"]
+print(lista[::])  # ["p", "y", "t", "h", "o", "n"]
+print(lista[::-1])  # ["n", "o", "h", "t", "y", "p"]
+
+# ITERAR LISTAS
+
+carros = ["gol", "celta", "palio"]
+
+for carro in carros:
+    print(carro)
+
+
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+# COMPREENSAO DE LISTAS
+# Filtrar lista
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Modificar valores
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+# APPEND
+
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+# CLEAR
+
+lista = [1, "Python", [40, 30, 20]]
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+lista.clear()
+
+print(lista)  # []
+
+# COPY
+
+lista = [1, "Python", [40, 30, 20]]
+
+lista.copy()
+
+print(lista)  # [1, "Python", [40, 30, 20]]
+
+# COUNT
+
+cores = ["vermelho", "azul", "verde", "azul"]
+
+print(cores.count("vermelho"))  # 1
+print(cores.count("azul"))  # 2
+print(cores.count("verde"))  # 1
+
+# EXTEND
+
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
+
+# INDEX
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("python"))  # 0
+
+# POP
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.pop())  # csharp
+print(linguagens.pop())  # java
+print(linguagens.pop())  # c
+print(linguagens.pop(0))  # python
+
+# REMOVE
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.remove("c")
+
+print(linguagens)  # ["python", "js", "java", "csharp"]
+
+# REVERSE
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+linguagens.reverse()
+
+print(linguagens)  # ["csharp", "java", "c", "js", "python"]
+
+# SORT
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort()  # ["c", "csharp", "java", "js", "python"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(reverse=True)  # ["python", "js", "java", "csharp", "c"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x))  # ["c", "js", "java", "python", "csharp"]
+print(linguagens)
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+linguagens.sort(key=lambda x: len(x), reverse=True)  # ["python", "csharp", "java", "js", "c"]
+print(linguagens)
+
+# LEN
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(len(linguagens))  # 5
+
+# SORTED
+
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(sorted(linguagens, key=lambda x: len(x)))  # ["c", "js", "java", "python", "csharp"]
+print(sorted(linguagens, key=lambda x: len(x), reverse=True))  # ["python", "csharp", "java", "js", "c"
